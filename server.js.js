da84895c4ef1db0f2b1632bdbc4822b8e2153c3f
cayce brown <caycebrown98@gmail.com>
2020-06-06 17:08:24 -0500

    added server.js and express app server build, modified script name in pkg.json

---
+++ server.js.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 2020
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  };
+
+  app.listen(PORT, function(err) {
+      if(err){throw err}
+      console.log(`Server has started on port ${PORT}`)
+  })
